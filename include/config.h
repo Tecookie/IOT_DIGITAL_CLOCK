c08276452a7d4242946a3695862be22b6241e1e2
Tecookie <quanglikecookie@gmail.com>
2025-11-14 17:43:52 +0700

    Update config.h

--- include/config.h
+++ include/config.h
@@ -14,7 +14,7 @@
 // ===== WiFi & Weather =====
 const char* WIFI_SSID = "";
 const char* WIFI_PASS = "";
-const String WEATHER_API_KEY = "76184842ab4678df905a4ce6fa399db2";
+const String WEATHER_API_KEY = "";
 const String CITY = "Ho%20Chi%20Minh";
 const unsigned long WEATHER_INTERVAL = 10 * 60 * 1000; // 10 minutes
 
